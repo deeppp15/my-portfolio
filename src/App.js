c8d1f5f7f18900190f883a976e0e7a74146fc431
Deep <voradeep15@gmail.com>
2023-05-14 21:52:06 -0400

    Added Files

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import react from "react";
+import "./index.css"
+import Home from "./routes/Home"
+import About from "./routes/About"
+import Project from "./routes/Project"
+import Contact from "./routes/Contact"
+import { Route, Routes } from "react-router-dom";
+
+//Parent file of Project
+//Call all pages here 
+//Create pages in routes
+//After that create components eg this nav bar, project setion, contact about section component folder
+function App() {
+  return (
+   <>
+   <Routes> /** Specifying routes for all pages  */
+    <Route path="/" element={<Home />} />/** Add address and path  */
+    <Route path="/project" element={<Project />} />/** Add address and path  */
+    <Route path="/about" element={<About />} />/** Add address and path  */
+    <Route path="/contact" element={<Contact />} />/** Add address and path  */
+   </Routes>
+   </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom"
+import "./NavbarStyles.css"
+import {FaBars,FaTimes } from "react-icons/fa";
+import React,{ useState } from 'react'
+;
+//  Portfolio header to go to home
+// Creating the navbar here 
+const Navbar = () => {
+    const[click,setClick]= useState(false);
+    const handleClick=()=> setClick(!click);
+
+  return (
+    <div className="header">
+        <Link to="/">
+            <h1>Portfolio</h1>         
+            </Link>
+            <ul className={click?"nav-menu active":"nav-menu"}>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/project">Project</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+            </ul>
+            <div className="hamburger" onclick={handleClick}>
+            {click ? (<FaTimes size={20} style={{ color: "#fff" }} />):
+            (<FaBars size={20} style={{ color: "#fff" }} />)}
+            </div>
+        </div>
+  )
+}
+
+export default Navbar
