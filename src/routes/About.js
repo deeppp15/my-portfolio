825c75ed7420060da47e47efe79bfcea31736cbb
Deep <voradeep15@gmail.com>
2023-05-15 02:18:45 -0400

    Added about, Contact and Project Pages

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import HeroImgProj from '../components/HeroImgProj'
 const About = () => {
   return (
-    <div>About</div>
+    <div>About
+        <Navbar/>
+        <HeroImgProj heading="ABOUT" text="I am a Full stack developer."/>
+        <Footer/>
+    </div>
   )
 }
 
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import HeroImgProj from '../components/HeroImgProj'
 const Project = () => {
   return (
-    <div>Project</div>
+    <div>Project
+     <Navbar/>
+     <HeroImgProj heading="PROJECTS."  text="Some of my most recent works"/>
+     <Footer/> 
+     </div>
   )
 }
 
