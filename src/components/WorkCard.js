0f6d9aadc8f33f19838243d0cf0f9e31435422f3
Deep <voradeep15@gmail.com>
2023-05-18 00:41:16 -0400

    Pushed card changes

---
+++ src/components/WorkCard.js
@@ -0,0 +1,20 @@
+import "./WorkCardStyles.css"
+
+
+import React from 'react'
+
+function WorkCard() {
+  return (
+    <div className="=work-container">
+        <h1 className="=work-container">Projects</h1>
+        
+        <div className="project-constainer">
+            <div className="project-card">
+                <img src="" alt="image" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default WorkCard
