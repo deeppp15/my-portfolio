cfe69e762071617537a3c98cd7ca0356613da693
Deep <voradeep15@gmail.com>
2023-05-18 23:44:03 -0400

    Added project card

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -2,15 +2,24 @@ import "./WorkCardStyles.css"
 
 
 import React from 'react'
-
+import dataImg from "../assets/dataMining.png"
+import { NavLink } from "react-router-dom"
 function WorkCard() {
   return (
     <div className="=work-container">
-        <h1 className="=work-container">Projects</h1>
-        
-        <div className="project-constainer">
+        <h1 className="project-heading">Projects</h1>
+        <div className="project-container">
             <div className="project-card">
-                <img src="" alt="image" />
+                <img src={dataImg} alt="image" />
+                <h2 className="project-title">Project Title</h2>
+                <div className="pro-details">
+                  <p> These are the Projects </p>
+                    <div className="pro-btns">
+                      <NavLink to="www.google.com" className="btn">View</NavLink>
+                      <NavLink to="www.google.com" className="btn">Source</NavLink>
+
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import HeroImgProj from '../components/HeroImgProj'
+import WorkCard from '../components/WorkCard'
 const Project = () => {
   return (
     <div>Project
      <Navbar/>
      <HeroImgProj heading="PROJECTS."  text="Some of my most recent works"/>
+     <WorkCard />
      <Footer/> 
      </div>
   )
