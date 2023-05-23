ca9f3cc71072db425cb55bbc91311d8cd462e9a1
Deep <voradeep15@gmail.com>
2023-05-23 17:35:36 -0400

    ADded Workcards

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -4,7 +4,7 @@ import "./WorkCardStyles.css"
 import React from 'react'
 import dataImg from "../assets/dataMining.png"
 import { NavLink } from "react-router-dom"
-function WorkCard() {
+const WorkCard =() =>{
   return (
     <div className="=work-container">
         <h1 className="project-heading">Projects</h1>
@@ -15,8 +15,10 @@ function WorkCard() {
                 <div className="pro-details">
                   <p> These are the Projects </p>
                     <div className="pro-btns">
-                      <NavLink to="www.google.com" className="btn">View</NavLink>
-                      <NavLink to="www.google.com" className="btn">Source</NavLink>
+                      <NavLink to="www.google.com" 
+                      className="btn">View</NavLink>
+                      <NavLink to="www.google.com" 
+                      className="btn">Source</NavLink>
 
                     </div>
                 </div>
