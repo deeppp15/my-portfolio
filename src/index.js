6f3b486f633cf24b5d52c150910dd49068b62d4f
Deep <voradeep15@gmail.com>
2023-05-28 17:26:57 -0400

    Changed tittle

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from "react-router-dom" 
-import ReactDOM from 'react-dom'
-
+import { BrowserRouter } from "react-router-dom" ;
+import ReactDOM from 'react-dom';
+import {Helmet} from 'react-helmet';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const TITLE = 'Deep Vora';
 root.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>
+  
+  <Helmet>
+  <title>{ TITLE }</title>
+</Helmet>
+</BrowserRouter>
 );
