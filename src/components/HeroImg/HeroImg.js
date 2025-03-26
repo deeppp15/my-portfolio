e24c851d3621bccffec290a926db118c86d06fb4
Deep Vora <voradeep15@gmail.com>
2025-03-25 21:11:19 -0400

    added badges

--- src/components/HeroImg/HeroImg.js
+++ src/components/HeroImg/HeroImg.js
@@ -4,7 +4,8 @@ import IntroImg from "../../assets/intro-bg.jpg"
 import { useTypingEffect } from "../../hooks/typing-effect"
 import { NavHashLink  } from "react-router-hash-link";
 import { Link } from "react-router-dom";
-
+import ai from "../../assets/aws-ai.png"
+import dva from "../../assets/aws-dva.png"
 
 const HeroImg = () => {
     
@@ -23,6 +24,14 @@ const HeroImg = () => {
                 <NavHashLink to="/my-portfolio/#contact"  smooth className="btn btn-light .btn-hover">Contact</NavHashLink>
             </div>
         </div>
+        <div className="badges">
+            <h1 style={{ textAlign: 'center' }} className="exp-heading">Certifications</h1>
+            <div className="badges-container">
+            <img src={dva} alt="AWS Technical Essentials" className="badge" />
+            <img src={ai} alt="AWS Cloud Practitioner" className="badge" />
+            </div>
+        </div>
+
     </div>
  
 }
